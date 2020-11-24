5c38f6b8873eb7a53f170b044010f7a6f30232e5
blacksmithop <>
2020-11-24 20:50:52 +0530

    MVP

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-import os
-from flask import Flask, flash, request, redirect, url_for, render_template
+from PIL import Image, ImageOps
+from flask import Flask, flash, request, redirect, render_template, send_file
 from werkzeug.utils import secure_filename
 from flask_assets import Environment, Bundle
+from io import BytesIO, StringIO
+
 
 UPLOAD_FOLDER = 'static\\uploads'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SESSION_TYPE'] = 'filesystem'
 app.secret_key = 'super secret key'
 
@@ -25,21 +26,23 @@ def allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
+
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
+
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('upload_file',
-                                    filename=filename))
+            im = Image.open(BytesIO(file.read()))
+            im = ImageOps.invert(image=im)
+            im_out = BytesIO()
+            im.save(im_out, 'JPEG')
+            im_out.seek(0)
+            return send_file(im_out, attachment_filename=f'invert_{filename}', as_attachment=True, mimetype='image/jpeg')
         else:
             flash(f"Only files of the type {', '.join(ALLOWED_EXTENSIONS)} are allowed")
     return render_template('index.html')
