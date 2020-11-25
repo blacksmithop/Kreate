2258cc8ad436a1f328dba96a33d7714ca2147dc8
blacksmithop <>
2020-11-25 10:28:28 +0530

    action response

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from PIL import Image, ImageOps
-from flask import Flask, flash, request, redirect, render_template, send_file
+from flask import Flask, flash, request, redirect, render_template, send_file, url_for
 from werkzeug.utils import secure_filename
 from flask_assets import Environment, Bundle
 from io import BytesIO, StringIO
 
 
 UPLOAD_FOLDER = 'static\\uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = -1
+
 app.secret_key = 'super secret key'
 
 assets = Environment(app)
@@ -28,12 +30,12 @@ def upload_file():
     if request.method == 'POST':
 
         if 'file' not in request.files:
-            flash('No file part')
+            flash('❌ No file part', 'error')
             return redirect(request.url)
         file = request.files['file']
 
         if file.filename == '':
-            flash('No selected file')
+            flash('❌ No file selected', 'info')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
@@ -42,11 +44,26 @@ def upload_file():
             im_out = BytesIO()
             im.save(im_out, 'JPEG')
             im_out.seek(0)
-            return send_file(im_out, attachment_filename=f'invert_{filename}', as_attachment=True, mimetype='image/jpeg')
+            flash("✅ Inverted image")
+            return send_file(im_out, attachment_filename=f'invert_{filename}',
+                             as_attachment=True, mimetype='image/jpeg')
         else:
-            flash(f"Only files of the type {', '.join(ALLOWED_EXTENSIONS)} are allowed")
+            flash(f"❓ Only files of the type {', '.join(ALLOWED_EXTENSIONS)} are allowed", 'warning')
+    return render_template('index.html')
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    flash('🛑 Unsupported operation', 'error')
+    return render_template('index.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    flash('🚫 Endpoint does not exist', 'error')
     return render_template('index.html')
 
 
 if __name__ == '__main__':
+    app.debug = False
     app.run()
